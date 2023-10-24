9aae05ae691967fda7ba1b10eaec2b9a3994aa57
Chirox03 <21520474@gm.uit.edu.vn>
2023-10-24 16:27:58 +0700

    ChatPage design

--- front-end/src/components/ChatPage/Conversation.js
+++ front-end/src/components/ChatPage/Conversation.js
@@ -1,9 +1,9 @@
 // Conversation.js
 import React from "react";
-
+import '../../App.css';
 const Conversation = () => {
   return (
-    <div className="flex flex-col flex-auto h-full p-6">
+    <div className="Conversation">
       <div className="flex flex-col h-full overflow-x-auto mb-4">
         <div className="flex flex-col h-full">
           <div className="grid grid-cols-12 gap-y-2">
@@ -11,10 +11,10 @@ const Conversation = () => {
             {/* Replace the placeholder content below with your conversation items */}
             <div className="col-start-1 col-end-8 p-3 rounded-lg">
               <div className="flex flex-row items-center">
-                <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+                <div className="flex items-center justify-center h-10 w-10 rounded-full bg-dark flex-shrink-0">
                   A
                 </div>
-                <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+                <div className="relative ml-3 text-sm bg-beige py-2 px-4 shadow rounded-xl">
                   <div>Hey How are you today?</div>
                 </div>
               </div>
