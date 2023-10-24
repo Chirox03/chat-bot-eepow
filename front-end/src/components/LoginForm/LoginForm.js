115b92737c24d6f2734d76c56b5148c747aa0a03
Chirox03 <21520474@gm.uit.edu.vn>
2023-10-24 16:06:16 +0700

    Log in page design

--- front-end/src/components/LoginForm/LoginForm.js
+++ front-end/src/components/LoginForm/LoginForm.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const LoginForm = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
-        <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">Login To Your Account</div>
-        <button className="relative mt-6 border rounded-md py-2 text-sm text-gray-800 bg-gray-100 hover:bg-gray-200">
+    <div className="LoginForm">
+      <div className="flex flex-col bg-light  shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
+        <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">Login To Eepow</div>
+        <button className="But">
           <span className="absolute left-0 top-0 flex items-center justify-center h-full w-10 text-blue-500">
             <i className="fab fa-facebook-f"></i>
           </span>
           <span>Login with Google</span>
         </button>
-        <div className="relative mt-10 h-px bg-gray-300">
+        <div className="relative mt-10 h-px bg-light">
           <div className="absolute left-0 top-0 flex justify-start w-full -mt-2">
-            <span className="bg-white px-4 text-xs text-gray-500 uppercase">Or Login With Username</span>
+            <span className="bg-light px-4 text-xs text-gray-500 uppercase">Or Login With Username</span>
           </div>
         </div>
         <div className="mt-10">
@@ -44,13 +44,13 @@ const LoginForm = () => {
             </div>
             <div className="flex items-center mb-6 -mt-4">
               <div className="flex ml-auto">
-                <button className="inline-flex text-xs sm:text-sm text-blue-500 hover:text-blue-700" type="button">
+                <button className="inline-flex text-xs sm:text-sm text-dark/90 hover:text-dark" type="button">
                   Forgot Your Password?
                 </button>
               </div>
             </div>
             <div className="flex w-full">
-              <button type="submit" className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
+              <button type="submit" className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-dark/90 hover:bg-dark rounded py-2 w-full transition duration-150 ease-in">
                 <span className="mr-2 uppercase">Login</span>
                 <span>
                   <svg className="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -62,7 +62,7 @@ const LoginForm = () => {
           </form>
         </div>
         <div className="flex justify-center items-center mt-6">
-          <button className="inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs text-center" type="button">
+          <button className="inline-flex items-center font-bold text-dark/80 hover:text-dark text-xs text-center" type="button">
             <span>
               <svg className="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        beige:'#F9F5F6',
+        light:'#F8E8EE',
+        medium:'#FDCEDF',
+        dark:'#F2BED1'
+      }
+    },
   },
   plugins: [],
 }
