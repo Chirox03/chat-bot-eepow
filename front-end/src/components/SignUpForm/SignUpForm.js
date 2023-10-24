3a8cf4427ccb52030665bf95cd90f5eb1e2cb229
Chirox03 <21520474@gm.uit.edu.vn>
2023-10-24 16:16:45 +0700

    Sign up form design

--- front-end/src/components/SignUpForm/SignUpForm.js
+++ front-end/src/components/SignUpForm/SignUpForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const LoginForm = () => {
+const SignUpForm = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
+    <div className="LoginForm">
+      <div className="flex flex-col bg-light shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
         <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">Sign up to Eepow</div>
-        <button className="relative mt-6 border rounded-md py-2 text-sm text-gray-800 bg-gray-100 hover:bg-gray-200">
+        <button className="But">
           <span className="absolute left-0 top-0 flex items-center justify-center h-full w-10 text-blue-500">
             <i className="fab fa-facebook-f"></i>
           </span>
@@ -13,7 +13,7 @@ const LoginForm = () => {
         </button>
         <div className="relative mt-10 h-px bg-gray-300">
           <div className="absolute left-0 top-0 flex justify-start w-full -mt-2">
-            <span className="bg-white px-4 text-xs text-gray-500 uppercase">Or Sign up With Username</span>
+            <span className="bg-light px-4 text-xs text-gray-500 uppercase">Or Sign up With Username</span>
           </div>
         </div>
         <div className="mt-10">
@@ -26,7 +26,7 @@ const LoginForm = () => {
                     <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                   </svg>
                 </div>
-                <input id="username" name="username" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Username" />
+                <input id="username" name="username" className="Input" placeholder="Username" />
               </div>
             </div>
 
@@ -40,7 +40,7 @@ const LoginForm = () => {
                     </svg>
                   </span>
                 </div>
-                <input id="password" type="password" name="password" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" />
+                <input id="password" type="password" name="password" className="Input" placeholder="Password" />
               </div>
             </div>
 
@@ -54,12 +54,12 @@ const LoginForm = () => {
                     </svg>
                   </span>
                 </div>
-                <input id="confirm-password" type="password" name="confirm-password" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Confirm Password" />
+                <input id="confirm-password" type="password" name="confirm-password" className="Input" placeholder="Confirm Password" />
               </div>
             </div>
 
             <div className="flex w-full">
-              <button type="submit" className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
+              <button type="submit" className="flex items-center justify-center focus:outline-none text-beige text-sm sm:text-base bg-dark/80 hover:bg-dark rounded py-2 w-full transition duration-150 ease-in">
                 <span className="mr-2 uppercase">Sign up</span>
                 <span>
                   <svg className="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -76,4 +76,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignUpForm;
