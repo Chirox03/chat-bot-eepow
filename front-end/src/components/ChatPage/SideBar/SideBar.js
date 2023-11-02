cdc7212348d22fcb920dfa167e3d17950b3d0458
Chirox03 <21520474@gm.uit.edu.vn>
2023-11-02 22:31:35 +0700

    add some button and modify the UI Q _ Q

--- front-end/src/components/ChatPage/SideBar/SideBar.js
+++ front-end/src/components/ChatPage/SideBar/SideBar.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Menu from "./Menu";
-
-const Sidebar = () => {
-  const handleLogout = () => {
-    // Perform logout functionality here
-  };
-
+import { Button } from "@material-tailwind/react";
+import { useState } from "react";
+const Sidebar = ({ conversations, activeConversation, onConversationClick }) => {
   return (
-    <div className="SideBar flex flex-col relative pt-6">
+    <div className="SideBar scrollbar-thin scrollbar-thumb-darker flex flex-col relative pt-6 md:w-64">
       <div className="flex flex-row items-center justify-center h-12 mar pt-6">
-        <div className="flex items-center justify-center rounded-2xl text-indigo-700 bg-dark h-10 w-10">
+        <div className="flex items-center justify-center rounded-2xl text-indigo-700 bg-dark h-10rem w-10rem">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -27,22 +24,33 @@ const Sidebar = () => {
         </div>
         <div className="ml-2 font-bold text-2xl">Eepow bot</div>
       </div>
+      <div className="w-full mt-5">
+      <Button size='lg' variant="outlined" fullWidth className="text-lg text-center py-2 h-10">New Chat +</Button>
+      </div >
       {/* Active Conversations */}
-      <div className="flex flex-col flex-grow mt-8">
-        <div className="flex flex-row items-center justify-between text-xs">
+      <div className="flex flex-grow overflow-y-auto flex-col flex-grow mt-4 mb-2">
+        <div className="flex flex-row items-center justify-between text-xs m-2">
           <span className="font-bold">Active Conversations</span>
         </div>
-        <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-          {/* Active conversation buttons */}
-          {/* Replace the placeholder content below with your active conversations */}
-          {/* Example button */}
-          <button className="flex flex-row items-center bg-light hover:bg-light/80 rounded-xl p-2">
-            <div className="flex items-center justify-center h-8 w-8 bg-beige rounded-full">H</div>
-            <div className="ml-2 text-sm font-semibold">Henry Boyd</div>
+        <div className="flex flex-col space-y-1 mt-4 ">
+        {conversations.map((conversation) => (
+          <button
+          onClick={() => onConversationClick(conversation.id)}
+          key={conversation.id}
+          className={
+            "flex flex-row items-center rounded-xl p-2 " +
+            (conversation.id === activeConversation.id ? "bg-light" : "bg-dark")
+          }
+          >
+            <div className="flex items-center justify-center h-8 w-8 bg-beige rounded-full">
+              {conversation.name?.charAt(0)}
+            </div>
+            <div className="ml-2 text-sm font-semibold">{conversation.name}</div>
           </button>
-          {/* Add more active conversation buttons as needed */}
+        ))}
         </div>
       </div>
+
       {/* Logout Button */}
      
       <div className="flex-grow absolute bottom-0 left-0 w-full">
